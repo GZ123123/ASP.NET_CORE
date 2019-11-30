cfe392a71f02447438010b286809df2c483c6905
PC_C502 <fakehuynhtruongchiquan@gmail.com>
2019-11-30 08:54:10 +0700

    Quân - Migrations

---
+++ QuanLyNhanSu/QuanLyNhanSu/Migrations/20191130015307_CreateTable.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuanLyNhanSu.Migrations
+{
+    public partial class CreateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PhongBan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MaPB = table.Column<string>(nullable: true),
+                    TenPB = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhongBan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NhanVien",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MaNV = table.Column<string>(nullable: true),
+                    MaPB = table.Column<string>(nullable: true),
+                    PhongBanId = table.Column<int>(nullable: true),
+                    TenNV = table.Column<string>(nullable: true),
+                    Luong = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NhanVien", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NhanVien_PhongBan_PhongBanId",
+                        column: x => x.PhongBanId,
+                        principalTable: "PhongBan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaiKhoan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TenDangNhap = table.Column<string>(nullable: true),
+                    MatKhau = table.Column<string>(nullable: true),
+                    MaPB = table.Column<string>(nullable: true),
+                    PhongBanId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaiKhoan", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaiKhoan_PhongBan_PhongBanId",
+                        column: x => x.PhongBanId,
+                        principalTable: "PhongBan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChamCong",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NgayLamViec = table.Column<DateTime>(nullable: false),
+                    DiemNhanVien = table.Column<int>(nullable: false),
+                    MaNV = table.Column<string>(nullable: true),
+                    NhanVienId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChamCong", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChamCong_NhanVien_NhanVienId",
+                        column: x => x.NhanVienId,
+                        principalTable: "NhanVien",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChamCong_NhanVienId",
+                table: "ChamCong",
+                column: "NhanVienId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NhanVien_PhongBanId",
+                table: "NhanVien",
+                column: "PhongBanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaiKhoan_PhongBanId",
+                table: "TaiKhoan",
+                column: "PhongBanId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChamCong");
+
+            migrationBuilder.DropTable(
+                name: "TaiKhoan");
+
+            migrationBuilder.DropTable(
+                name: "NhanVien");
+
+            migrationBuilder.DropTable(
+                name: "PhongBan");
+        }
+    }
+}
