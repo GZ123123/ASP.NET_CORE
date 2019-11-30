6bad3454c06b13d390fb05a07b40d74790861447
Thong <thong1999ni@gmail.com>
2019-11-30 11:07:47 +0700

    CRUD nhan su

--- QuanLyNhanSu/QuanLyNhanSu/Controllers/NhanSuController.cs
+++ QuanLyNhanSu/QuanLyNhanSu/Controllers/NhanSuController.cs
@@ -3,32 +3,151 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using QuanLyNhanSu.Data;
+using QuanLyNhanSu.Models;
 
 namespace QuanLyNhanSu.Controllers
 {
     public class NhanSuController : Controller
     {
-        private MyDbContext _context;
+        private readonly MyDbContext _context;
+
         public NhanSuController(MyDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+
+        // GET: NhanSu
+        public async Task<IActionResult> Index()
         {
-            return View(_context.NhanVien.ToList());
+            return View(await _context.NhanVien.ToListAsync());
         }
+
+        // GET: NhanSu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanVien);
+        }
+
+        // GET: NhanSu/Create
         public IActionResult Create()
         {
             return View();
         }
-        public IActionResult Edit()
+
+        // POST: NhanSu/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MaNV,MaPB,TenNV,Luong")] NhanVien nhanVien)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(nhanVien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhanVien);
         }
-        public IActionResult Delete()
+
+        // GET: NhanSu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return RedirectToAction("Index");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien.FindAsync(id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // POST: NhanSu/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MaNV,MaPB,TenNV,Luong")] NhanVien nhanVien)
+        {
+            if (id != nhanVien.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nhanVien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhanVienExists(nhanVien.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nhanVien);
+        }
+
+        // GET: NhanSu/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanVien);
+        }
+
+        // POST: NhanSu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nhanVien = await _context.NhanVien.FindAsync(id);
+            _context.NhanVien.Remove(nhanVien);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NhanVienExists(int id)
+        {
+            return _context.NhanVien.Any(e => e.Id == id);
         }
     }
 }
