72bdd947f90692dec9b99273ece058b4fbe7c930
Mailfox97 <50673953+Mailfox97@users.noreply.github.com>
2019-11-30 09:59:07 +0700

    Finish view phòng ban và nhân sự

--- QuanLyNhanSu/QuanLyNhanSu/Controllers/NhanSuController.cs
+++ QuanLyNhanSu/QuanLyNhanSu/Controllers/NhanSuController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuanLyNhanSu.Data;
 
 namespace QuanLyNhanSu.Controllers
 {
     public class NhanSuController : Controller
     {
+        private MyDbContext _context;
+        public NhanSuController(MyDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.NhanVien.ToList());
         }
         public IActionResult Create()
         {
